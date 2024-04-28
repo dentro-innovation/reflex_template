2252992ba514e8562500e8727c00f671f5749678
Paul <your.email@example.com>
2024-04-28 20:26:13 -0300

    add logging

---
+++ reflex_template/__init__.py
@@ -0,0 +1,27 @@
+import logging
+from .utils.logging_config import CustomFormatter
+import os
+from logtail import LogtailHandler
+
+
+def configure_logger():
+    logger = logging.getLogger()  # root logger
+    log_level = logging.getLevelName(os.environ.get("LOG_LEVEL", "DEBUG"))
+    logger.setLevel(log_level)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(os.environ.get("LOG_LEVEL", logging.DEBUG))
+    stream_handler.setFormatter(CustomFormatter())
+
+    logger.addHandler(stream_handler)
+
+    if os.environ.get("BETTERSTACK_API_TOKEN"):
+      betterstack_handler = LogtailHandler(
+          source_token=os.environ.get("BETTERSTACK_API_TOKEN")
+      )
+      logger.addHandler(betterstack_handler)
+
+    logging.getLogger("watchfiles.main").setLevel(logging.WARNING)
+
+
+configure_logger()
--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,7 @@
 import reflex as rx
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 config = rx.Config(
     app_name="reflex_template",
 )
