fe140d5e58dacc4505060da9bd18d76afd8202b9
Paul <your.email@example.com>
2024-04-28 20:14:52 -0300

    initial reflex setup plus production deployment files and github deployment action

---
+++ reflex_template/reflex_template.py
@@ -0,0 +1,36 @@
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+
+from rxconfig import config
+
+import reflex as rx
+
+docs_url = "https://reflex.dev/docs/getting-started/introduction/"
+filename = f"{config.app_name}/{config.app_name}.py"
+
+
+class State(rx.State):
+    """The app state."""
+
+
+def index() -> rx.Component:
+    return rx.center(
+        rx.theme_panel(),
+        rx.vstack(
+            rx.heading("Welcome to Reflex!", size="9"),
+            rx.text("Get started by editing ", rx.code(filename)),
+            rx.button(
+                "Check out our docs!",
+                on_click=lambda: rx.redirect(docs_url),
+                size="4",
+            ),
+            rx.logo(),
+            align="center",
+            spacing="7",
+            font_size="2em",
+        ),
+        height="100vh",
+    )
+
+
+app = rx.App()
+app.add_page(index)
